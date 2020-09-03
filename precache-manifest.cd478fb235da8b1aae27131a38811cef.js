1503888f6e3fb9c3404dc52e3c609ae49ad7afa4
Adnan <adnan.khan@wwindia.com>
2020-09-03 18:27:27 +0530

    Updates

---
+++ precache-manifest.cd478fb235da8b1aae27131a38811cef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589ec4030f0d1ade4d1e3dc9e9c2f282",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "043b6e5cbb6966bd31fd",
+    "url": "/React-Todo/static/css/main.bf2b281d.chunk.css"
+  },
+  {
+    "revision": "f8532f07598d7df48c61",
+    "url": "/React-Todo/static/js/2.7c57fa15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo/static/js/2.7c57fa15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043b6e5cbb6966bd31fd",
+    "url": "/React-Todo/static/js/main.5f4c1a28.chunk.js"
+  },
+  {
+    "revision": "499e3a2da2a321831db3",
+    "url": "/React-Todo/static/js/runtime-main.62a11df1.js"
+  }
+]);
